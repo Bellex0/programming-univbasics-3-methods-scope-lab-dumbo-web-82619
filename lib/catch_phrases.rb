b3dc306bebeca44cc0d9fbe44a775e2182ed7649
Belle P <bellepx3@gmail.com>
2019-07-11 01:09:11 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,13 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  phrase_2 = "It's Dangerous To Go Alone! Take This."
+  puts phrase_2
+end
+
+def any_phrase(phrase)
+  phrase = "Do A Barrel Roll!"
+  puts phrase
+end
